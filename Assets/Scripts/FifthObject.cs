19a029da548a02e5a9aff9b635e7978938c34982
DIFIKYT <68289492+DIFIKYT@users.noreply.github.com>
2024-08-11 15:46:20 +0700

    Commit

---
+++ Assets/Scripts/FifthObject.cs
@@ -0,0 +1,20 @@
+using DG.Tweening;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FifthObject : MonoBehaviour
+{
+    [SerializeField] private List<Color> _colors;
+    [SerializeField] private int _colorChangeDuration;
+    [SerializeField] private MeshRenderer _renderer;
+
+    private Color currrentColor = Color.white;
+
+    private void Update()
+    {
+        if (_renderer.material.color != currrentColor)
+            _renderer.material.DOColor(currrentColor, _colorChangeDuration);
+        else
+            currrentColor = _colors[Random.Range(0, _colors.Count)];
+    }
+}
---
+++ Assets/Scripts/SixthObject.cs
@@ -0,0 +1,19 @@
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SixthObject : MonoBehaviour
+{
+    [SerializeField] private Text _text;
+    [SerializeField] private float _textChangeDuration;
+
+    private void Start()
+    {
+        Sequence sequence = DOTween.Sequence();
+
+        sequence.Append(_text.DOText("Замена текста", _textChangeDuration));
+        sequence.Append(_text.DOText("Дополнение текста", _textChangeDuration).SetRelative());
+        sequence.Append(_text.DOColor(Color.green, 0));
+        sequence.Append(_text.DOText("Взлом текста", _textChangeDuration, true, ScrambleMode.Numerals));
+    }
+}
